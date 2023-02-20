fe36d5cf6a25552871af0bc6dd8af31747ba494e
CodeWithThanun <cc.thanun@gmail.com>
2023-02-20 15:53:23 +0700

    first comit

---
+++ src/utils/Data.js
@@ -0,0 +1,27 @@
+const services = [
+  {
+    title: "Free Shipping",
+    tagline: "From all over 5$",
+    Image: "images/service.png",
+  },
+  {
+    title: "Daily Surprise Offer",
+    tagline: "Save Upto 25% off",
+    Image: "images/service-02.png",
+  },
+  {
+    title: "Support 24/7",
+    tagline: "Shop with expert",
+    Image: "images/service-03.png",
+  },
+  {
+    title: "Affordable Price",
+    tagline: "get best price",
+    Image: "images/service-04.png",
+  },
+  {
+    title: "Secure Payments",
+    tagline: "100% protected",
+    Image: "images/service-05.png",
+  },
+];
